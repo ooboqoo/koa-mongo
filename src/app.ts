c604210f38ed8ad254336f6e06da580d831f4ff0
Gavin Wang <ooboqoo@gmail.com>
2017-01-18 10:28:35 +0800

    fix wrong logger package version

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 // Koa and its middlewares still using commonjs format, ES6 module format will throw error after typescript complilation.
 const Koa = require('koa');
 const config = require('config');
-const logger = require('koa-logger');
 
 import middleware from './middlewares';
 import routes from './routes';
 
 const app = new Koa();
 
-app.use(logger());
 app.use(middleware());
 app.use(routes());
 
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,6 @@
 const compose = require('koa-compose');  // Compose the given middleware and return middleware.
 const convert = require('koa-convert');  // convert koa legacy generator middleware to promise middleware ( v2.x )
+const logger = require('koa-logger');
 const cors = require('koa-cors');
 const bodyParser = require('koa-bodyparser');
 
@@ -7,6 +8,7 @@ import handleErrors from './handle-errors';
 
 export default function middleware() {
   return compose([
+    logger(),
     handleErrors(),
     convert(cors()),
     bodyParser(),
