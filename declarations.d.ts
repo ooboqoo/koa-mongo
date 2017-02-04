c4c195f93f07cff439eddeceb50cdbf4b8ff310c
Gavin Wang <ooboqoo@gmail.com>
2017-02-04 11:33:36 +0800

    clean unnecessary file

--- declarations.d.ts
+++
@@ -1,6 +0,0 @@
-/*
- * A wildcard module is declared below to allow third party libraries to be used in an app 
- * even if they don't provide their own type declarations.
- */
-
-declare module '*';
