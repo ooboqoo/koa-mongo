6bab97e1f2d965871b1230d331326355aeb40fc0
Gavin Wang <ooboqoo@gmail.com>
2017-03-18 17:56:28 +0800

    fix: add NodeJS patch of  btoa atob function

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,5 +1,15 @@
 export { AES } from 'crypto-js';
 
+// node-browser-compat/btoa
+function btoa(str) {
+  return new Buffer(str).toString('base64');  // browser implement not support UTF-8 but this works
+}
+
+// node-browser-compat/atob
+function atob(str) {
+  return new Buffer(str, 'base64').toString('binary');
+}
+
 export class Base64 {
   encrypt(p: string, k = 'gYn') {
     return btoa(k.split('').reverse().join('') + btoa(p));
