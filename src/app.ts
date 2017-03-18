12420a8c85948a68902195774d0f7ffb72db6bd0
Gavin Wang <ooboqoo@gmail.com>
2017-03-18 17:54:55 +0800

    feat: add color for console output

--- src/app.ts
+++ src/app.ts
@@ -13,20 +13,20 @@ app.use(routes());
 
 attachSocketIO(app);
 
-(async() => {
+(async () => {
   try {
     const dbUrl = config.get<string>('dbUrl');
     const info = await connectDatabase(dbUrl);
-    console.log(`Connected to ${dbUrl}`);
+    console.info('\x1b[32m%s\x1b[0m', `Connected to ${dbUrl}`);
   } catch (error) {
-    console.error('Unable to connect to database', error);
+    console.error('\x1b[31m%s\n%s\x1b[0m', 'Unable to connect to database.', error);
   }
 
   try {
     const port = config.get<string>('port');
     await app.listen(port);
-    console.info(`Listening to http://localhost:${port}`);
+    console.info('\x1b[32m%s\x1b[0m', `Listening to http://localhost:${port}`);
   } catch (error) {
-    console.log(error);
+    console.error('\x1b[31m%s\x1b[0m', error);
   }
 })();
